6cfab2a0e051a30565816c37b4773c5b8462ec88
XINGHAO LI <lixingh@deakin.edu.au>
2019-11-20 14:41:34 +1100

    add remove duplicate function for public

--- tests/utility.test.js
+++ tests/utility.test.js
@@ -1,5 +1,6 @@
 const cheerio = require('cheerio');
 const queryOrder = require('../utility/http');
+const utility = require('../utility/utility');
 const host = 'http://www.eol.cn/e_html/gk/fsx/index.shtml';
 test.skip('test http get', () => {
     expect.assertions(1);
@@ -8,4 +9,11 @@ test.skip('test http get', () => {
         var len = $('body > div.fsx > div.center > div.fsshow.clearfix>div>div.tline > div > table').length;
         expect(len > 0).toEqual(true);
     });
+});
+
+test('test the remove duplicate public function', ()=>{
+    var arr = utility.removeDuplicate([1,1,2,2,3]);
+    var arr1 = utility.removeDuplicate(12);
+    expect(arr.length == 3).toEqual(true);
+    expect(arr1 == -1).toEqual(true);
 });
---
+++ utility/utility.js
@@ -0,0 +1,15 @@
+class Utility{
+
+    static removeDuplicate(arr){
+        if(!Array.isArray(arr)){
+            return -1;
+        }
+        var set = new Set(arr);
+        return [...set];
+    }
+
+
+
+}
+
+module.exports = Utility;
