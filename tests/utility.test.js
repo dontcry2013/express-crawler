5875ca93c8c92b00b1eff48ec8581f9137d00940
Zac <zach.qin@gmail.com>
2019-11-22 11:12:21 +1100

    add isset as public function to check property of object

--- tests/utility.test.js
+++ tests/utility.test.js
@@ -17,3 +17,19 @@ test('test the remove duplicate public function', ()=>{
     expect(arr.length == 3).toEqual(true);
     expect(arr1 == -1).toEqual(true);
 });
+
+test('test the isset function', ()=>{
+    utility.isset(() => some); // false
+
+    // Defining objects
+    let some = { nested: { value: 'hello' } };
+
+    // More tests that never throw an error
+    expect(utility.isset(() => some)).toBe(true); // true
+    expect(utility.isset(() => some.nested)).toBe(true); // true
+    expect(utility.isset(() => some.nested.value)).toBe(true); // true
+    expect(utility.isset(() => some.nested.deeper.value)).toBe(false); // false
+
+    // Less compact but still viable except when trying to use `this` context
+    expect(utility.isset(function () { return some.nested.deeper.value })).toBe(false); // false
+});
--- utility/utility.js
+++ utility/utility.js
@@ -8,7 +8,22 @@ class Utility{
         return [...set];
     }
 
-
+    /**
+     * Checks to see if a value is set.
+     *
+     * @param {Function} accessor Function that returns our value
+     */
+    static isset (accessor) {
+        try {
+            // Note we're seeing if the returned value of our function is not
+            // undefined
+            return typeof accessor() !== 'undefined';
+        } catch (e) {
+            // And we're able to catch the Error it would normally throw for
+            // referencing a property of undefined
+            return false;
+        }
+    }
 
 }
 
