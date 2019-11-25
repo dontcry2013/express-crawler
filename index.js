084e309184f09a3ff9ec9a7236c2b949a05af945
CHENCHEN TAN <tanchen@deakin.edu.au>
2019-11-25 22:25:20 +1100

    Four loops are used and four functions are defined, get an array of 1776 pieces of data. But the code no exception handling. it need further improvment.

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ var cheerio = require('cheerio');
 var host = 'http://www.eol.cn/e_html/gk/fsx/index.shtml';
 var queryOrder = require('./utility/http');
 var utility = require('./utility/utility.js');
-console.log(utility)
-// process.exit();
+
+
 queryOrder(host, 11).then(function(arr){
     var $ = cheerio.load(arr[0]);
     var result=[];
-    $('.fsshowli').each((i, v)=>{
-
-        var record=[];
-        record.push(utility.getPriId());
-        record.push(utility.getYear());
-        utility.PushScoreAndLevel(record,result);     
-})
-    console.log(result);
+for(var j=1;j<7;j++){//每一个省份都有六年的数据，并且六个表的其实selector是从1开始的，所以循环六次
+ $('.fsshowli').each((i, v)=>{//第二层循环 遍历每一个省份的六个表，并获得其中一个省份的表
+        
+        var PriID=utility.getPriId(v);
+        var year=utility.getYear(j);// 调用函数获得省份和年份
+        var trs =$(v).find('div.tline > div:nth-child('+j+')>table .tr-cont')//得到六个表中其中一个的所有tr
+        var level=0;
+        trs.each((ii, vv)=>{  //第三层循环遍历（除了表头）tr(每一行)
+            $(vv).find('td').each((iii, vvv)=>{ //第四层循环遍历一行中的每一个td
+            if(iii==0){//如果索引为0则值为批次存进变量level中
+                level=utility.getLevel(iii,vvv);
+            }
+            if(iii>0){ //当iii不为0是则将前面所得的数据存入一个数组中
+            var record=[];  
+            record.push(PriID);
+            record.push(year);  
+            utility.pushScoreAndDivision(iii,vvv,record);//获取分数和文（理）科
+            record.push(level);
+            result.push(record);//将一个得到的每一条数据存入数组中
+            }
+                
+            })
+         }) 
+        })
+        
+    }
+    console.log(result);//获得所有的数据（一个二维数组）
 })
 
 // process.exit()
--- utility/utility.js
+++ utility/utility.js
@@ -1,79 +1,47 @@
+var cheerio = require('cheerio');
+const data = require('../testdata.js');
+
 class Utility{
-<<<<<<< HEAD
-   static getPriId(){
     
-        var pri=$(div).find('.city').text();
+    //查找省份  输入：V= <div class="fsshowli" id="bj">...</div> 输出（返回）：北京
+   static getPriId(v){
+        var $ = cheerio.load(data);
+        var pri=$(v).find('.city').text();
       //  $.each(privince,function(key,value){
 		//if (key==pri)
-      //  record.push(value);
+      //  record.push(value);//与map连接
 
        // })
        return pri;
     }
-
-   static getYear(){
-        var year=$('div.sline.clearfix > div').text();
+    
+    //获取年份 例：输入：j=1 输出（返回）：2019
+   static getYear(j){
+        var $ = cheerio.load(data);
+        var year=$('#bj>div.sline.clearfix > div:nth-child('+j+')').text();
+        year=year.substr(0,year.length-1);//除去‘年’字
         return year;
     }
-    static getLevelAndScore(record,iii,vvv){
-                if(iii == 0){
-                    var level = $(vvv).text()
-                    record.push(level);
-                    return;
-                } else if(iii==1){
-                    var arr = []
-                    arr.push('art')
-                    arr.push($(vvv).text())
-                } else{
-                    var arr = []
-                    arr.push('science')
-                    arr.push($(vvv).text())
-                }         
-        return arr;
-            
+
+    //获取批次 例：输入：iii=0; vvv= 本科批 输出（返回）：本科批
+    static getLevel(iii,vvv){
+        var $ = cheerio.load(data);                   
+        var level = $(vvv).text();
+        return level;
     }
 
-      static  PushScoreAndLevel(record,reslut){
-            var trs =$(v).find('table .tr-cont')
-            trs.each((ii, vv)=>{
-                $(vv).find('td').each((iii, vvv)=>{
-                   record.push(this.getLevelAndScore(record,iii,vvv));
-                   reslut.push(record);
-                })
-                
-            })
-
+    //获取分数及文科或者理科 例：输入： iii=1,vvv=480，record={北京，2019} 输出：record{北京，2019,480，art}
+    static  pushScoreAndDivision(iii,vvv,record){
+        var $ = cheerio.load(data);
+        if(iii==1){
+            record.push($(vvv).text());
+            record.push('art');//如果td在tr中的第二列，则为文科，并将这两项放入record数组中                                          
+        } else{
+            record.push($(vvv).text());
+            record.push('science');   //如果td在tr中的第三列，则为理科，并将这两项放入record数组中                       
+        }   
+        return record;                  
     }
 
 }
-=======
-
-    static removeDuplicate(arr){
-        if(!Array.isArray(arr)){
-            return -1;
-        }
-        var set = new Set(arr);
-        return [...set];
-    }
-
-    /**
-     * Checks to see if a value is set.
-     *
-     * @param {Function} accessor Function that returns our value
-     */
-    static isset (accessor) {
-        try {
-            // Note we're seeing if the returned value of our function is not
-            // undefined
-            return typeof accessor() !== 'undefined';
-        } catch (e) {
-            // And we're able to catch the Error it would normally throw for
-            // referencing a property of undefined
-            return false;
-        }
-    }
-
-}
-
->>>>>>> 2542c26d13baaf3fca59b7bf419d24b220353e42
 module.exports = Utility;
