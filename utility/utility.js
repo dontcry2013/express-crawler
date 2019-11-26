8656577c3c8cbef57fedeb08cb5e6013064375b2
roughChilli <296733347@qq.com>
2019-11-26 12:51:24 +1100

    add escapeHTML adn demo functions in utility (#7)
    approves pull request

--- utility/utility.js
+++ utility/utility.js
@@ -25,6 +25,31 @@ class Utility{
         }
     }
 
+    static getCategory(age) {  
+        var category = '';  
+        switch (true) {  
+        case isNaN(age):  
+            category = 'not an age';  
+            break;  
+        case (age >= 50):  
+            category = 'Old';  
+            break;  
+        case (age <= 20):  
+            category = 'Baby';  
+            break;  
+        default:  
+            category = 'Young';  
+            break;  
+        }
+        return category;  
+    }  
+
+    static escapeHTML(text) {  
+        var replacements= {'<': '&lt;', '>': '&gt;','&': '&amp;', '"': '&quot;'};                      
+        return text.replace(/[<>&"]/g, function(character) {  
+            return replacements[character];  
+        });
+    }
 }
 
 module.exports = Utility;
