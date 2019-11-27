1e5a4e46a0ba6eae828ec8c07fec5f113ac11ff2
CHENCHEN TAN <tanchen@deakin.edu.au>
2019-11-27 14:29:37 +1100

    get score and division the art and science

--- index.js
+++ index.js
@@ -6,17 +6,39 @@ var queryOrder = require('./utility/http');
 
 queryOrder(host, 11).then(function(arr){
     var $ = cheerio.load(arr[0]);
-    var obj=[];
-    $('body > div.fsx > div.center > div.fsshow.clearfix>div>div.tline > div > table > tbody > tr.tr-cont').each(function(index, div){
-        var level = $(div).find('td:nth-child(1)').text();
-        obj.push(level);
-    });
-    var set = new Set(obj);   
-    var ar = [...set];
-    ar = ar.filter(function(item){
-        return (item != '-' && item != '' & item != ' ');
-    });
-    console.log(ar);
-});
+    var result=[];
+for(var j=1;j<7;j++){//每一个省份都有六年的数据，并且六个表的其实selector是从1开始的，所以循环六次
+ $('.fsshowli').each((i, v)=>{//第二层循环 遍历每一个省份的六个表，并获得其中一个省份的表
+        
+        var PriID=utility.getPriId(v);
+        var year=utility.getYear(j);// 调用函数获得省份和年份
+        var trs =$(v).find('div.tline > div:nth-child('+j+')>table .tr-cont')//得到六个表中其中一个的所有tr
+        var level=0;
+        trs.each((ii, vv)=>{  //第三层循环遍历（除了表头）tr(每一行)
+            $(vv).find('td').each((iii, vvv)=>{ //第四层循环遍历一行中的每一个td
+            
+            //过滤所有的无用数据
+            if($(vvv).text()!="-"&&$(vvv).text()!=""&&$(vvv).text()!=" "&&/点击查看/.test($(vvv).text())==false){
+            if(iii==0){//如果索引为0则值为批次存进变量level中
+                level=$(vvv).text();              
+                }
+            if(j<4&&(PriID=='上海'||PriID=='浙江')){//特殊处理2019-2017上海和浙江的分数
+                if(iii>0)
+                    if($(vvv).text()!='分数线'&&$(vvv).text()!='综合'){
+                    result.push(utility.specialPush(PriID,year,level,iii,vvv));//将一个得到的每一条数据存入数组中
+                   }
+                }
+            else{//处理文理分科的表
+                if(iii>0)//当iii不为0是则将前面所得的数据存入一个数组中            
+                    //获取分数和文（理）科
+                    result.push(utility.pushScoreAndDivision(PriID,year,level,iii,vvv));//将一个得到的每一条数据存入数组中                      
+                }
+            }
+            })
+         }) 
+        })   
+    }
+    console.log(result);//获得所有的数据（一个二维数组）
+})
 
 // process.exit()
--- utility/utility.js
+++ utility/utility.js
@@ -25,6 +25,23 @@ class Utility{
         }
     }
 
+    //获取分数及文科或者理科 例：输入： iii=1,vvv=480，record={北京，2019} 输出：record{北京，2019,480，art}
+    static  pushScoreAndDivision(PriID,year,level,iii,vvv){
+        var $ = cheerio.load(data);
+        var record=[];
+        record.push(PriID);
+        record.push(year);  
+        if(iii==1){
+            record.push($(vvv).text());
+            record.push('art');//如果td在tr中的第二列，则为文科，并将这两项放入record数组中                                          
+        } else{
+            record.push($(vvv).text());
+            record.push('science');   //如果td在tr中的第三列，则为理科，并将这两项放入record数组中                       
+        }   
+        record.push(level);
+        return record;                  
+    }
+    
     static getCategory(age) {  
         var category = '';  
         switch (true) {  
