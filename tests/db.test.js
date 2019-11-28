18efe51188253ff3366a0e6999c8a54d1c05c44d
Zach <296733347@qq.com>
2019-11-28 11:40:00 +1100

    add db test case

--- tests/db.test.js
+++ tests/db.test.js
@@ -2,7 +2,7 @@ const DB = require('../utility/db');
 var db = new DB();
 
 test('mysql select prepare', () => {
-    expect(DB.prepare()).toBe('SELECT `*` FROM `users` WHERE `id` = 1');
+    expect(DB.prepare()).toBe('SELECT `*` FROM `users`');
     expect(DB.prepare(['id', 'name'], 'level', 100)).toBe('SELECT `id`, `name` FROM `level` WHERE `id` = 100');
 });
 
@@ -17,7 +17,6 @@ test('mysql select test', (done) => {
 test('should throw an error if result is not 2 [ASYNC/AWAIT]', async (done) => {
     try {
         var test = await db.asyncQuery('select 1+1');
-        console.log('test:', test);
         expect(typeof test).toBe('object');
         expect(test[0]['1+1']).toBe(2);
         done();
@@ -25,6 +24,24 @@ test('should throw an error if result is not 2 [ASYNC/AWAIT]', async (done) => {
         expect(error.message).toBe('User with id: 11 was not found.');
     }
 });
+
+// a demo for Lauren
+test('should get the admission level map', async (done) => {
+    try {
+        var admissionLevelPromise = db.getPromiseOfAdmissionLevel();
+        await db.handleGetPromiseOfAdmissionLevel(admissionLevelPromise);
+        console.log(db.admissionLevelMap);
+        expect(db.admissionLevelMap['自主招生线']).toBe(2);
+        expect(db.admissionLevelMap['三批']).toBe(11);
+        expect(db.admissionLevelMap['第三批']).toBe(60);
+        expect(db.admissionLevelMap['二批B']).toBe(89);
+        expect(db.admissionLevelMap['牛奶']).toBe(undefined);
+        done();
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
 
 afterAll(() => {
     db.dbClose();
