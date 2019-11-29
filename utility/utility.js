768705815c8e1bedb66a5cfeda9a6606ac3da98b
CHENCHEN TAN <tanchen@deakin.edu.au>
2019-11-29 14:35:44 +1100

    Get the expected result but no test

--- utility/utility.js
+++ utility/utility.js
@@ -4,7 +4,7 @@ const data = require('../testdata.js');
 class Utility{
     
     //查找省份  输入：V= <div class="fsshowli" id="bj">...</div> 输出（返回）：北京
-    static getPriId(v){
+    static getPriId(v,map){
         var $ = cheerio.load(data);
         var pri=$(v).find('.city').text();
         try{
@@ -16,7 +16,12 @@ class Utility{
         }catch (error){
             console.error('The format of privince is not expected');
         }
-        return pri;
+        for(var i in map) {
+            if (i==pri){
+                return parseInt(map[i]); 
+            }    
+       }
+        
     }
     
     //获取年份 例：输入：j=1 输出（返回）：2019
@@ -26,14 +31,22 @@ class Utility{
         year=parseInt(year.substr(0,year.length-1));//除去‘年’字
         return year;
     }
-
+    //传入批次名以及储存批次的对象，获取该批次的ID 例：输入：本科批，map 输出：1（number类型）
+    static getLevel(map,levelValue){
+        for(var i in map) {
+            if (i==levelValue){
+                return parseInt(map[i]);   
+            }  
+       }
+    }
     //处理没有文理分科的表
-    static specialPush(PriID,year,level,iii,vvv){
+    static specialPush(PriID,year,level,vvv){
         var $ = cheerio.load(data);
         var record=[];
+        var score=parseInt($(vvv).text().substr(0,3));
         record.push(PriID);
         record.push(year);         
-        record.push($(vvv).text());
+        record.push(score);
         record.push('all');//如果td在tr中的第二列，则为文科，并将这两项放入record数组中                                          
         record.push(level);
         return record;   
@@ -42,14 +55,14 @@ class Utility{
     //获取分数及文科或者理科 例：输入： iii=1,vvv=480，record={北京，2019} 输出：record{北京，2019,480，art}
     static  pushScoreAndDivision(PriID,year,level,iii,vvv){
         var $ = cheerio.load(data);
+        var score=$(vvv).text().substr(0,3);
         var record=[];
         record.push(PriID);
         record.push(year);  
-        if(iii==1){
-            record.push($(vvv).text());
+        record.push(score);
+        if(iii==1){          
             record.push('art');//如果td在tr中的第二列，则为文科，并将这两项放入record数组中                                          
-        } else{
-            record.push($(vvv).text());
+        }else{
             record.push('science');   //如果td在tr中的第三列，则为理科，并将这两项放入record数组中                       
         }   
         record.push(level);
@@ -82,5 +95,7 @@ class Utility{
         });
     }
 }
+
+
 
 module.exports = Utility;
