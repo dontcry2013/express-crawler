c46c01d7ed9fafc3b9d2cae1da78150d368ead90
Zac <zach.qin@gmail.com>
2019-12-11 14:46:52 +1100

    modify http to remove /r/n/t

--- utility/http.js
+++ utility/http.js
@@ -3,16 +3,18 @@ const http = require('http');
 const queryOrder = function getUrlContentWithHttpGet() {
   // eslint-disable-next-line prefer-rest-params
   const arg = arguments;
+  let str = '';
   return new Promise(((resolve) => {
     http.get(arg[0], (res) => {
-      const chunks = [];
       res.on('data', (chunk) => {
-        chunks.push(chunk);
+        let chunkStr = chunk.toString();
+        chunkStr = chunkStr.replace(/\r?\n|\r|\t/g, ' ');
+        str += chunkStr;
       });
       res.on('end', () => {
-        const buffer = Buffer.concat(chunks);
+        // const buffer = Buffer.concat(chunks);
         console.log(`**********************${arg[0]}**********************`);
-        resolve([buffer.toString(), arg[1]]);
+        resolve([str, arg[1]]);
       });
     });
   }));
